d4a79f697644815bf5bf27c52b8dcc3550ea4d9d
Venkata Yamajala <syamajala@gmail.com>
2015-06-19 09:33:58 -0400

    Working neville's algorithm.

--- Approximation/neville.h
+++ Approximation/neville.h
@@ -6,38 +6,33 @@
 using namespace std;
 
 template <class T>
-using Node =  SharedBinaryNode<T>;
-template <class T>
-using NodePtr = shared_ptr< SharedBinaryNode<T> >;
-
-template <class T>
 class Neville : public Fit<T> {
- private:  
-  SharedBinaryTree<T> tree;
-
-  void build_tree(const T point, const int deg) {
-    vector<int> nnIdx = this->neighbors(point, deg+1);
-    vector< vector< NodePtr<T> > > nodes;
+ private:
+  vector<vector<T>> nodes;
+  
+  void build_table(const T point, const int deg) {
+    vector<int> nnIdx = this->neighbors(point, deg);
+    //    vector<vector<T>> nodes;
     vector<T> sx;
 
     // n = 0
-    vector< NodePtr<T> > zero_nodes;
+    vector<T> zero_nodes;
     for(auto i : nnIdx) {
       sx.push_back(this->x[i]);
-      zero_nodes.push_back(make_shared<Node<T>>(this->f[i]));
+      zero_nodes.push_back(this->f[i]);
     }
     nodes.push_back(zero_nodes);
 
     int depth = 1;
     for(int n = 1; n < deg+1; n++) {
-      vector< NodePtr<T> > n_nodes;
+      vector<T> n_nodes;
+
       for(size_t i = 0; i < nodes[n-1].size() - 1; i++) {
-	T a = (point - sx[i])*(nodes[n-1][i+1]->get_val());
-	T b = (point - sx[i+n])*(nodes[n-1][i]->get_val());
+	T a = (point - sx[i])*(nodes[n-1][i+1]);
+	T b = (point - sx[i+n])*(nodes[n-1][i]);
 	T c = (sx[i+n] - sx[i]);
 	T val = (a - b)/c;
-	cout << "f(" << i << ", " << n << ") = " << val << endl; 
-	n_nodes.push_back(make_shared<Node<T>>(val));
+	n_nodes.push_back(val);
       }
       nodes.push_back(n_nodes);
       depth++;
@@ -48,10 +43,10 @@ class Neville : public Fit<T> {
  Neville(const vector<T> &x, const vector<T> &f) : Fit<T>(x, f) {};
 
   function<T (T)> fit(const T point, const int deg) {
-    this->build_tree(point, deg);
+    this->build_table(point, deg);
 
-    function<T (T)> poly = [](T _x) -> T {
-      return 0.0;
+    function<T (T)> poly = [this](T _x) -> T {
+      return (nodes.back()).back();
     };
     return poly;
   };
